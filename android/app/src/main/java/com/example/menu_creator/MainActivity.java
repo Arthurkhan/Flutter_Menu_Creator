1a8f62c90fcd1b5a28bceda3a8566abe99abce1b
Arthurkhan <arthurokhan@gmail.com>
2025-04-11 12:42:11 +0700

    Add MainActivity.java with v2 embedding

---
+++ android/app/src/main/java/com/example/menu_creator/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.menu_creator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // You can keep this empty for most cases.
+    // This class is the main entry point for your Flutter application on Android.
+}
